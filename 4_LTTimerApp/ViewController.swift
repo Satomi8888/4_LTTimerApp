ca69ab94874afae62f45010953eb6046433c3217
遠山聡美 <S083@inv-238.inv.local>
2018-03-08 10:06:43 +0900

    Initial Commit

---
+++ 4_LTTimerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4_LTTimerApp
+//
+//  Created by 遠山　聡美 on 2018/03/08.
+//  Copyright © 2018年 Simple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
